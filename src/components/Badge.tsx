2b7a4ca4c2e68b7b1ad1ec3f7f7797b85262fa8b
rizzioli <rizzioli@oaservice.it>
2023-11-10 17:28:13 +0100

    background 7

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -12,29 +12,29 @@ export const Badge = (props: Props) => {
   let liteBadge: HTMLAnchorElement | undefined;
   let observer: MutationObserver | undefined;
 
-  const appendBadgeIfNecessary = (mutations: MutationRecord[]) => {
-    mutations.forEach((mutation) => {
-      mutation.removedNodes.forEach((removedNode) => {
-        if (
-          "id" in removedNode &&
-          liteBadge &&
-          removedNode.id == "lite-badge"
-        ) {
-          console.log("Sorry, you can't remove the brand 😅");
-          props.botContainer?.append(liteBadge);
-        }
-      });
-    });
-  };
+  // const appendBadgeIfNecessary = (mutations: MutationRecord[]) => {
+  //   mutations.forEach((mutation) => {
+  //     mutation.removedNodes.forEach((removedNode) => {
+  //       if (
+  //         "id" in removedNode &&
+  //         liteBadge &&
+  //         removedNode.id == "lite-badge"
+  //       ) {
+  //         console.log("Sorry, you can't remove the brand 😅");
+  //         props.botContainer?.append(liteBadge);
+  //       }
+  //     });
+  //   });
+  // };
 
-  onMount(() => {
-    if (!document || !props.botContainer) return;
-    observer = new MutationObserver(appendBadgeIfNecessary);
-    observer.observe(props.botContainer, {
-      subtree: false,
-      childList: true,
-    });
-  });
+  // onMount(() => {
+  //   if (!document || !props.botContainer) return;
+  //   observer = new MutationObserver(appendBadgeIfNecessary);
+  //   observer.observe(props.botContainer, {
+  //     subtree: false,
+  //     childList: true,
+  //   });
+  // });
 
   onCleanup(() => {
     if (observer) observer.disconnect();
@@ -68,7 +68,7 @@ export const Badge = (props: Props) => {
           color: props.poweredByTextColor ?? defaultTextColor,          
         }}
       >
-        <span> Oa Rizzio Service</span>
+        <span> Oa Rizzio Cosa Belo Service</span>
       </a>
     </span>
   );
